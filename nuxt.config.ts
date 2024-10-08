0ae61e7e2d5f93966f70c932cd10b7320c805093
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2024-10-08 18:25:23 +0200

    feat: use csr instead of ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module"],
+  ssr: false,
   // vuetify: {
   //   moduleOptions: {
   //     /* module specific options */
