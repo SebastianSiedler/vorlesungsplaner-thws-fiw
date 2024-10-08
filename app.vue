b4049f2cf6724c4639de92b50347d6878b7943bb
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2024-10-08 19:25:44 +0200

    feat: classes table instead of list

--- app.vue
+++ app.vue
@@ -30,46 +30,84 @@
     <!-- Button to download iCal -->
     <v-btn @click="downloadCalendar">Download iCal</v-btn>
 
-    <v-text-field label="Query" v-model="query"></v-text-field>
+    <!-- Vuetify Data Table to show events -->
+    <v-data-table
+      :headers="headers"
+      :items="events.state.value"
+      :search="search"
+      item-value="id"
+      :loading="events.isLoading.value"
+      @update:options="loadItems"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Classes</v-toolbar-title>
+          <!-- Search Field for the Data Table -->
+          <v-text-field
+            v-model="search"
+            label="Search Events"
+            clearable
+          ></v-text-field>
+        </v-toolbar>
+      </template>
 
-    <v-list>
-      <v-list-item v-for="event in events.state.value" :key="event.id">
-        {{ event.name }}
-        {{ event.lecturerNames }}
-        {{ event.moduleNumber }}
+      <template v-slot:item.actions="{ item }">
+        <v-btn @click="() => selectedEvents.push(item)" color="primary" small>
+          Add
+        </v-btn>
+      </template>
 
-        <v-btn
-          @click="
-            () => {
-              selectedEvents.push(event);
-            }
-          "
-          >Add</v-btn
-        >
-      </v-list-item>
-    </v-list>
+      <template v-slot:item.studyGroups="{ value }">
+        <div>
+          <v-chip v-for="group in value.split(',')">
+            {{ group }}
+          </v-chip>
+        </div>
+      </template>
+
+      <template v-slot:item.upcomingSubEventDate="{ value }">
+        {{ new Date(value).toLocaleString("de") }}
+      </template>
+
+      <template v-slot:loading>
+        <v-skeleton-loader type="table-row@10"></v-skeleton-loader>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, reactive } from "vue";
+import { ref, watch } from "vue";
 import { client, type Event } from "./fiwClient";
 import { useAsyncState } from "@vueuse/core";
-// import { writeFileSync } from 'browser-fs-access';
-import ical from "ical-generator";
+import { VDataTable } from "vuetify/components";
 
 // State
 const selectedEvents = ref<Event[]>([]);
-const query = ref("");
+const search = ref("");
+
+// Table headers
+const headers: VDataTable["$props"]["headers"] = [
+  { title: "Event Name", value: "name" },
+  { title: "Lecturer", value: "lecturerNames" },
+  { title: "Module Number", value: "moduleNumber" },
+  { title: "studyGroups", value: "studyGroups" },
+  { title: "upcomingSubEventDate", value: "upcomingSubEventDate" },
+  { title: "Actions", value: "actions", sortable: false },
+];
 
 // API call to fetch events
-const events = useAsyncState(
-  async () => {
-    return await client.getEvents({ q: query.value });
-  },
-  [],
-  { throwError: true }
-);
+const events = useAsyncState(client.getEvents, [], { throwError: true });
+
+const loadItems: VDataTable["$props"]["onUpdate:options"] = (args) => {
+  const { page, itemsPerPage, search } = args;
+  console.log(args);
+  events.execute(0, {
+    q: search,
+    size: itemsPerPage === -1 ? 1000 : itemsPerPage,
+    offset: (page - 1) * itemsPerPage,
+  });
+};
 
 const icalUrl = computed(() => {
   return (
@@ -78,10 +116,6 @@ const icalUrl = computed(() => {
       .map((event) => event.id)
       .join(",")}`
   );
-});
-
-watch(query, () => {
-  events.execute();
 });
 
 const downloadCalendar = () => {
